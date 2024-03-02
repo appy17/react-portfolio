7ae3fd0d44a9614fe46b6f1c87cc3ecfb96dc21c
sprabhat3 <singhprabhat96@gmail.com>
2024-03-03 01:16:23 +0530

    prabhat

---
+++ src/content_option.js
@@ -0,0 +1,166 @@
+const logotext = "Apeksha";
+const meta = {
+    title: "Apeksha Tembhurne",
+    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+    title: "I’m Apeksha Tembhurne",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop websites",
+    },
+    description: "Apeksha Tembhurne, I'm a software developer",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "About My Self",
+    aboutme: "Experienced Engineering Graduate with a passion for transitioning to the IT industry. Skilled in project management, problem-solving, and technical communication. Recognized for ability to work collaboratively in team environments, and adapt to new challenges. Seeking opportunities to apply analytical skills and knowledge of technology to drive innovation and success. Dedicated to continuous learning and professional development.",
+};
+const worktimeline = [{
+        jobtitle: "Full Stak Developer(Intern)",
+        where: "Techno Geeks",
+        date: "2022",
+    },
+    {
+        jobtitle: "React(Front End Developer) Intern",
+        where: "SS CComputer Nagpur",
+        date: "2023",
+    },
+    {
+        jobtitle: "Front End Developer",
+        where: "Royal WebTech Pvt. Ltd. ",
+        date: "Current",
+    },
+];
+
+const skills = [{
+        name: "Core Java",
+        value: 80,
+    },
+    {
+        name: "Spring",
+        value: 70,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "HTML/CSS",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "To accomplish this, I follow a user-centered design approach that involves gathering user requirements, creating wireframes and prototypes, and conducting user testing. I use design tools such as Adobe XD, Sketch, or Figma to craft interfaces that are visually appealing and easy to navigate.",
+    },
+    {
+        title: "Front End Developement",
+        description: "I used to develope Front-end by using a variety of technologies and tools to build the visual and interactive components of a website or web application. This includes HTML, CSS, and JavaScript, as well as front-end frameworks and libraries like React and Angular",
+    },
+    {
+        title: "Wordpress Design",
+        description: "As a WordPress designer, I specialize in creating custom WordPress websites that are both visually appealing and user-friendly. With years of experience in designing and building WordPress websites, I have a deep understanding of popular WordPress themes and plugins, and how to use them to create unique designs that meet my clients' needs.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "apekshatembhurne17@gmail.com",
+    YOUR_FONE: "(+91)9403575320",
+    description: "I'm Front End Developer and You can fill these details to contact me for work related discussion.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/appy17",
+    facebook: "https://facebook.com",
+    linkedin: "https://www.linkedin.com/in/apeksha-tembhurne/",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
